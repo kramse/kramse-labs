dcfef4cfc4e131d2cce7e4cda6845bf4e117b9ee
Henrik Kramselund <hlk@kramse.org>
2023-03-01 10:41:42 +0100

    dirty fix

--- work-station/volatility-install.sh
+++ work-station/volatility-install.sh
@@ -8,3 +8,5 @@
 sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
 sudo python3 -m pip install -U distorm3 yara yara-python pycrypto pillow openpyxl ujson pytz ipython capstone
 sudo python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
+
+sudo ln -s /usr/local/lib/python3*/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
