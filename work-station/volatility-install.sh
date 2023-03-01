4bd27427320d9b8599467bcb649addc2e71a19ae
Henrik Kramselund <hlk@kramse.org>
2023-03-01 10:32:50 +0100

    New tool

--- work-station/volatility-install.sh
+++ work-station/volatility-install.sh
@@ -8,6 +8,3 @@
 sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
 sudo python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
 sudo python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
-
-
-echo "export PATH=/home/$USER/.local/bin:\$PATH" >> ~/.bashrc
