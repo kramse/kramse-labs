7150bf71ec0757adf62e79f9220d83c579affccc
Henrik Kramselund <hlk@kramse.org>
2023-03-01 10:36:47 +0100

    Missing dependencies

--- work-station/volatility-install.sh
+++ work-station/volatility-install.sh
@@ -6,5 +6,5 @@
 
 
 sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
-sudo python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+sudo python3 -m pip install -U distorm3 yara yara-python pycrypto pillow openpyxl ujson pytz ipython capstone
 sudo python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
